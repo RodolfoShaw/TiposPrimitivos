acbc94fd6dae36cf4f8d6979a19dd3acca126293
RodolfoShaw <rodolfobatista922@gmail.com>
2023-03-03 21:47:17 -0300

    segundo commit

---
+++ src/tiposprimitivos/Constantes.java
@@ -0,0 +1,10 @@
+package tiposprimitivos;
+
+public class Constantes {
+    final Double Acelaracao_da_gravidade = 10.0;
+    
+    
+
+    System.out.println("Valor da constante: " + Acelaracao_da_gravidade)
+}
+
