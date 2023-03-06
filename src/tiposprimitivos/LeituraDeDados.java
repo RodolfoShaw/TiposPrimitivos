2fff96859167ac8938795bd2758f081ea3e90655
RodolfoShaw <rodolfobatista922@gmail.com>
2023-03-05 21:02:19 -0300

    terceiro commit

---
+++ src/tiposprimitivos/LeituraDeDados.java
@@ -0,0 +1,11 @@
+package tiposprimitivos;
+
+import java.util.Scanner;
+
+public class LeituraDeDados {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+    
+}
